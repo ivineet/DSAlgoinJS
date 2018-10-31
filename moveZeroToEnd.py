cf81b35f8c4a87464f2d7f820d172db1900adeee
Vineet Yadav <ivineet@users.noreply.github.com>
2018-11-01 01:09:05 +0530

    Add files via upload

--- moveZeroToEnd.py
+++ moveZeroToEnd.py
@@ -1,12 +1,21 @@
-Python 3.6.4rc1 (v3.6.4rc1:3398dcb, Dec  5 2017, 20:41:32) [MSC v.1900 64 bit (AMD64)] on win32
-Type "copyright", "credits" or "license()" for more information.
->>> 
-=============== RESTART: F:/projects/DS_Algo/moveZeroToEnd.py ===============
-[1, 2, 4, 5, 8, 9, 6, 7, 0, 0, 0, 0, 0]
->>> 
-=============== RESTART: F:/projects/DS_Algo/moveZeroToEnd.py ===============
-[1, 2, 3, 4]
->>> 
-=============== RESTART: F:/projects/DS_Algo/moveZeroToEnd.py ===============
-[1, 2, 3, 4, 0, 0]
->>> 
+def moveZeroestoEnd(arr1, n):
+    count = 0
+
+    for i in range(n):
+        if(arr1[i] != 0):
+            arr1[count] = arr1[i]
+            count += 1
+
+    while count < n:
+        arr1[count] = 0
+        count += 1
+
+    print(arr1)
+
+
+if __name__ == "__main__":
+
+    arr = [1,2,3,4,0,0,0,3,0,9,0]
+    n = len(arr)
+
+    moveZeroestoEnd(arr, n)
