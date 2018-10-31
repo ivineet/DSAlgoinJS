ae07fc955318f1ae94cc55fa0bff2689fbf09d45
unknown <ivineetyadav@gmail.com>
2018-11-01 00:47:55 +0530

    move all zeroes to end in an array

---
+++ moveZeroToEnd.py
@@ -0,0 +1,12 @@
+Python 3.6.4rc1 (v3.6.4rc1:3398dcb, Dec  5 2017, 20:41:32) [MSC v.1900 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+=============== RESTART: F:/projects/DS_Algo/moveZeroToEnd.py ===============
+[1, 2, 4, 5, 8, 9, 6, 7, 0, 0, 0, 0, 0]
+>>> 
+=============== RESTART: F:/projects/DS_Algo/moveZeroToEnd.py ===============
+[1, 2, 3, 4]
+>>> 
+=============== RESTART: F:/projects/DS_Algo/moveZeroToEnd.py ===============
+[1, 2, 3, 4, 0, 0]
+>>> 
